ecb516de765d37e2a912dc99ea639a9c5af7e696
Zeyad_Absattar <zeyadabdelsattar@gmail.com>
2024-07-11 04:07:07 +0300

    Initialize firebase in the project

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,19 @@
 import 'package:banter/screens/sign_in_screen.dart';
 import 'package:banter/screens/sign_up_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const Banter());
 }
 
 class Banter extends StatelessWidget {
-  const Banter({Key? key});
+  const Banter({super.key});
 
   @override
   Widget build(BuildContext context) {
